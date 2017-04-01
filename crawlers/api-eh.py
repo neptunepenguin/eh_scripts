fff54ca95f526aad7b053542406ef427ed3ceeea
neptunepenguin <john.valentine.1984@gmail.com>
2017-04-01 17:28:40 +0100

    made the API compatible with both python2 and python3

--- crawlers/api-eh.py
+++ crawlers/api-eh.py
@@ -2,7 +2,13 @@
 
 # simple EH api test
 
-import json, urllib, httplib
+import json, urllib, sys
+
+# python 2 vs. python 3 compatibility
+try:
+    from http import client as httpclient
+except ImportError:
+    import httplib as httpclient
 
 reqpy = { "method"  : "gdata"
         , "gidlist" : [ [ 639967 , "e2be237948" ]
@@ -11,13 +17,25 @@ reqpy = { "method"  : "gdata"
 headers = { "Accept"       : "application/jsonrequest"
           , "Content-type" : "text/json"
           }
+if 1 < len(sys.argv):
+    gidlist = []
+    for arg in sys.argv[1:]:
+        if '/' in arg:
+            left, right = arg.split('/')
+            left = int(left)
+            gidlist.append([left, right])
+        else:
+            print('Bad arg:', arg)
+    print(gidlist)
+    reqpy['gidlist'] = gidlist
 req   = json.dumps(reqpy)
-conn  = httplib.HTTPConnection("e-hentai.org")
+conn  = httpclient.HTTPConnection("e-hentai.org")
 conn.request("POST", "/api.php", req, headers);
 reply = conn.getresponse()
 data  = reply.read()
-print reply.status, reply.reason
-print data
+print(reply.status, reply.reason)
+print(data)
 response = json.loads(data)
+print(response)
 conn.close()
 
