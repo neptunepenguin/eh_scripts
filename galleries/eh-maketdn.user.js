033270cdd8b4f7653848e09b716285e77bf8e2af
neptunepenguin <john.valentine.1984@gmail.com>
2021-05-19 17:29:36 +0100

    added a downvote request maker

---
+++ galleries/eh-maketdn.user.js
@@ -0,0 +1,111 @@
+// ==UserScript==
+// @name        eh-maketdn
+// @namespace   e-hentai
+// @description generates a downvote request for the gallery
+// @include     http://e-hentai.org/g/*
+// @include     https://e-hentai.org/g/*
+// @include     https://exhentai.org/g/*
+// @version     0.1
+// @grant       none
+// ==/UserScript==
+/*
+@usage_start
+
+The script provides a clickable, semi-transparent box in the top left corner of
+the page.  Clicking the box opens a dialog containing text about all the tags
+you have voted down in the current gallery.  The text is formatted in a way
+easy to copy paste into the Tagging and Abuse thread.
+
+Q&A
+
+1. Can you automate the posting to the thread?
+No, do not want to make it easier for uselessly spamming the thread.
+
+2. Can you make a request for upvotes too?
+No, we have enough people already who will upvote it without checking anyway.
+
+@usage_end
+
+@licstart
+
+eh-maketdn.user.js - downvote request maker
+
+Copyright (C) 2021 Aquamarine Penguin
+
+This JavaScript code is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License (GNU GPL) as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+This JavaScript code is distributed WITHOUT ANY WARRANTY; without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+the GNU GPL for more details.
+
+The full text of the license can be found in the COPYING file.  If you cannot
+find this file, see <http://www.gnu.org/licenses/>.
+
+@licend
+*/
+
+"use strict";
+
+(function () {
+    var div = document.createElement('div');
+    var div_style = 'opacity:0.7;position:fixed;z-index:100;top:0;left:0;';
+    div_style += 'padding:20px;border-radius:3px;border:2px solid white;';
+    div_style += 'text-align:left;font-size:10pt;background:white;';
+    div.style = div_style;
+    document.body.appendChild(div);
+    div.style.cursor = 'pointer';
+
+    function getTagText(el) {
+        var ns = el.parentNode.parentNode.parentNode.firstChild.textContent;
+        return ns + el.textContent;
+    }
+
+    function makeTdnList(e) {
+        console.log('show');
+        var innerdiv = document.createElement('div');
+        var downvotes = document.getElementsByClassName('tdn');
+        var loc = window.location;  // just in case
+        var url = loc.protocol + '//' + loc.host + loc.pathname;
+        var urltext = document.createTextNode(url);
+        var urlp = document.createElement('p');
+        urlp.appendChild(urltext);
+        innerdiv.appendChild(urlp);
+        var i, j, arr, chunk = 3;
+        for (i=0, j=downvotes.length; i<j; i+=chunk) {
+            arr = Array.prototype.slice.call(downvotes, i, i+chunk);
+            var p = document.createElement('p');
+            arr = arr.map(getTagText);
+            var txt = document.createTextNode(arr.join(' , '));
+            p.appendChild(txt);
+            innerdiv.appendChild(p);
+        }
+        var hide = document.createElement('div');
+        hide.style = 'text-align:center;font-size:12pt;padding:30px;';
+        hide.style.cursor = 'pointer';
+        hide.appendChild(document.createTextNode('HIDE'))
+        div.removeEventListener('click', makeTdnList);
+        hide.addEventListener('click', hideTdnList);
+        innerdiv.appendChild(hide);
+        div.appendChild(innerdiv);
+        div.style.cursor = 'auto';
+    }
+
+    function hideTdnList(e) {
+        console.log('hide');
+        e.target.removeEventListener('click', hideTdnList);  // just in case
+        var innerdiv = e.target.parentNode;
+        div.removeChild(innerdiv);
+        e.stopPropagation();
+        div.addEventListener('click', makeTdnList);
+        div.style.cursor = 'pointer';
+    }
+
+    div.addEventListener('click', makeTdnList);
+})();
+
+// useful to tell us if something blew up
+console.log('eh-maketdn is active');
+
